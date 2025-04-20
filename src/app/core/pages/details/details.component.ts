a1793518818bf29a9aebe156f720f4ba814de3ca
Michael Breitkreutz <mbreitkr16@gmail.com>
2025-04-20 12:40:29 -0600

    Refactored and styled details page

--- src/app/core/pages/details/details.component.ts
+++ src/app/core/pages/details/details.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+import { BikeDetailsComponent } from "../../../features/bikes/components/bike-details/bike-details.component";
+import { BackButtonComponent } from "../../../shared/components/back-button/back-button.component";
 
 @Component({
-  selector: 'app-details',
-  imports: [],
-  templateUrl: './details.component.html',
-  styleUrl: './details.component.scss'
+  selector: "app-details",
+  imports: [BikeDetailsComponent, BackButtonComponent],
+  templateUrl: "./details.component.html",
+  styleUrl: "./details.component.scss",
 })
-export class DetailsComponent {
-
-}
+export class DetailsComponent {}
