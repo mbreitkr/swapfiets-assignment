462de9fa3c219dbae58c29de2ebd7fa9c8677684
Michael Breitkreutz <mbreitkr16@gmail.com>
2025-04-19 21:08:57 -0600

    BikeSearchInput component creation (WIP)

---
+++ src/app/features/bikes/components/bike-search-input/bike-search-input.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BikeSearchInputComponent } from './bike-search-input.component';
+
+describe('BikeSearchInputComponent', () => {
+  let component: BikeSearchInputComponent;
+  let fixture: ComponentFixture<BikeSearchInputComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BikeSearchInputComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BikeSearchInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/bikes/components/bike-search-input/bike-search-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bike-search-input',
+  imports: [],
+  templateUrl: './bike-search-input.component.html',
+  styleUrl: './bike-search-input.component.scss'
+})
+export class BikeSearchInputComponent {
+
+}
