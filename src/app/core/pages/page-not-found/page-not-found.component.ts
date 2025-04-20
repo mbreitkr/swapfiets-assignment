31bef1c64c0606334aacfdcaf27d59fb357c385a
Michael Breitkreutz <mbreitkr16@gmail.com>
2025-04-20 17:58:31 -0600

    Completed first pass of Details page and PageNotFound page

--- src/app/core/pages/page-not-found/page-not-found.component.ts
+++ src/app/core/pages/page-not-found/page-not-found.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+import { BackButtonComponent } from "../../../shared/components/back-button/back-button.component";
 
 @Component({
-  selector: 'app-page-not-found',
-  imports: [],
-  templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.scss'
+  selector: "app-page-not-found",
+  imports: [BackButtonComponent],
+  templateUrl: "./page-not-found.component.html",
+  styleUrl: "./page-not-found.component.scss",
 })
-export class PageNotFoundComponent {
-
-}
+export class PageNotFoundComponent {}
