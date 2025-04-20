696e71309054feb1a9f23ec5826736113be10363
Michael Breitkreutz <mbreitkr16@gmail.com>
2025-04-20 09:51:20 -0600

    Created Footer component; added footer component to App layout

--- src/app/core/layouts/app-layout/app-layout.component.ts
+++ src/app/core/layouts/app-layout/app-layout.component.ts
@@ -2,10 +2,11 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
 import { HeaderComponent } from "../../../shared/components/header/header.component";
+import { FooterComponent } from "../../../shared/components/footer/footer.component";
 
 @Component({
   selector: "app-app-layout",
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: "./app-layout.component.html",
   styleUrl: "./app-layout.component.scss",
 })
