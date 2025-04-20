b3267ab68ba9757c7b27d2e3c9b9e78fea0da07a
Michael Breitkreutz <mbreitkr16@gmail.com>
2025-04-20 12:27:47 -0600

    Created BikeDetails and BackButton components

---
+++ src/app/features/bikes/components/bike-details/bike-details.component.ts
@@ -0,0 +1,10 @@
+import { NgOptimizedImage } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-bike-details",
+  imports: [NgOptimizedImage],
+  templateUrl: "./bike-details.component.html",
+  styleUrl: "./bike-details.component.scss",
+})
+export class BikeDetailsComponent {}
