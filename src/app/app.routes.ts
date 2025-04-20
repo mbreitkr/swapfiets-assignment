9dbb1c9dd29708f395b48910298d22c3427da16f
Michael Breitkreutz <mbreitkr16@gmail.com>
2025-04-19 21:17:33 -0600

    Added nested routing for layouts, update AppComponent to reflect changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+import { SearchComponent } from "./core/pages/search/search.component";
+import { DetailsComponent } from "./core/pages/details/details.component";
+import { PageNotFoundComponent } from "./core/pages/page-not-found/page-not-found.component";
+import { AppLayoutComponent } from "./core/layouts/app-layout/app-layout.component";
+
+export const routes: Routes = [
+  {
+    path: "",
+    component: AppLayoutComponent,
+    children: [
+      { path: "search", component: SearchComponent },
+      { path: "details", component: DetailsComponent },
+      { path: "", redirectTo: "/search", pathMatch: "full" },
+      { path: "**", component: PageNotFoundComponent },
+    ],
+  },
+  {
+    path: "**",
+    redirectTo: "",
+  },
+];
