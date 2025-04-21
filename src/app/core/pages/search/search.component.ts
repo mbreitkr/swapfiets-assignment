04896e637bc700330b24ed5138c9fa9063d77745
Michael Breitkreutz <mbreitkr16@gmail.com>
2025-04-21 17:16:00 -0600

    Replaced page contents with new BikeSearch component

--- src/app/core/pages/search/search.component.ts
+++ src/app/core/pages/search/search.component.ts
@@ -1,10 +1,9 @@
 import { Component } from "@angular/core";
-import { BikeSearchInputComponent } from "../../../features/bikes/components/bike-search-input/bike-search-input.component";
-import { BikeSearchResultsComponent } from "../../../features/bikes/components/bike-search-results/bike-search-results.component";
+import { BikeSearchComponent } from "../../../features/bikes/components/bike-search/bike-search.component";
 
 @Component({
   selector: "app-search",
-  imports: [BikeSearchInputComponent, BikeSearchResultsComponent],
+  imports: [BikeSearchComponent],
   templateUrl: "./search.component.html",
   styleUrl: "./search.component.scss",
 })
