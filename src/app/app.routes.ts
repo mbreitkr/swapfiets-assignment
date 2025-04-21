4ba91396ae57b4106ab669582e47159af949c4d8
Michael Breitkreutz <mbreitkr16@gmail.com>
2025-04-20 18:17:44 -0600

    Added lazy loading to search and details routes; added id param to details route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from "@angular/router";
 
-import { SearchComponent } from "./core/pages/search/search.component";
-import { DetailsComponent } from "./core/pages/details/details.component";
 import { PageNotFoundComponent } from "./core/pages/page-not-found/page-not-found.component";
 import { AppLayoutComponent } from "./core/layouts/app-layout/app-layout.component";
 
@@ -9,11 +7,26 @@ export const routes: Routes = [
   {
     path: "",
     component: AppLayoutComponent,
+    title: "App Layout",
     children: [
-      { path: "search", component: SearchComponent },
-      { path: "details", component: DetailsComponent },
+      {
+        path: "search",
+        loadComponent: () =>
+          import("./core/pages/search/search.component").then(
+            (c) => c.SearchComponent
+          ),
+        title: "Bike Search",
+      },
+      {
+        path: "details/:id",
+        loadComponent: () =>
+          import("./core/pages/details/details.component").then(
+            (c) => c.DetailsComponent
+          ),
+        title: "Bike Details",
+      },
       { path: "", redirectTo: "/search", pathMatch: "full" },
-      { path: "**", component: PageNotFoundComponent },
+      { path: "**", component: PageNotFoundComponent, title: "Page Not Found" },
     ],
   },
   {
