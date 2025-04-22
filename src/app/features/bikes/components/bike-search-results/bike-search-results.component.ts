508500a0e44aeb76d9d2437d3f75c288eda81825
Michael Breitkreutz <mbreitkr16@gmail.com>
2025-04-22 09:07:16 -0600

    Initial pagination add and styling (not wired in)

--- src/app/features/bikes/components/bike-search-results/bike-search-results.component.ts
+++ src/app/features/bikes/components/bike-search-results/bike-search-results.component.ts
@@ -1,5 +1,6 @@
 import { Component, input } from "@angular/core";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatPaginatorModule } from "@angular/material/paginator";
 
 import { BikeSearchResultCardComponent } from "../bike-search-result-card/bike-search-result-card.component";
 
@@ -7,7 +8,11 @@ import { BikeSummary } from "../../interfaces/bike.model";
 
 @Component({
   selector: "app-bike-search-results",
-  imports: [BikeSearchResultCardComponent, MatProgressSpinnerModule],
+  imports: [
+    BikeSearchResultCardComponent,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+  ],
   templateUrl: "./bike-search-results.component.html",
   styleUrl: "./bike-search-results.component.scss",
 })
