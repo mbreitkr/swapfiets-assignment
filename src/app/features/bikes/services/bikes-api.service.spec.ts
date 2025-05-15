58f075329efab0cfa19e7adf191cdda9afc3bb09
Michael Breitkreutz <mbreitkr16@gmail.com>
2025-05-15 11:24:05 -0600

    Add initial BikeApi service and BikeSearchResultCard component unit tests (copied files from separate local branch due to conflicts with interview pair programming changes to BikeSearch component)

--- src/app/features/bikes/services/bikes-api.service.spec.ts
+++ src/app/features/bikes/services/bikes-api.service.spec.ts
@@ -1,16 +1,218 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import { HttpErrorResponse, provideHttpClient } from "@angular/common/http";
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from "@angular/common/http/testing";
 
-import { BikesApiService } from './bikes-api.service';
+import { BikesApiService } from "./bikes-api.service";
 
-describe('BikesApiService', () => {
-  let service: BikesApiService;
+import {
+  BIKE_INDEX_V3_API_BASE_URL,
+  BIKE_SEARCH_RESULTS_PER_PAGE,
+} from "../../../core/constants/api.config";
+import { expectStaticBikeSearchParams } from "../testing/bike-api-test-helpers";
+import { bikeSummariesMock } from "../testing/mocks/bike-search-results.mock";
+import { bikeDetailsMock } from "../testing/mocks/bike-details.mock";
+import { bikeResultsCountMock } from "../testing/mocks/bike-results-count.mock";
+
+describe("BikesApiService", () => {
+  let bikeApiService: BikesApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(BikesApiService);
+    TestBed.configureTestingModule({
+      providers: [
+        BikesApiService,
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    });
+    bikeApiService = TestBed.inject(BikesApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe("getBikesByCity", () => {
+    const resultsPerPage = BIKE_SEARCH_RESULTS_PER_PAGE;
+
+    it("should return a valid page of search results", () => {
+      // SETUP
+      const city = "Amsterdam";
+      const pageNumber = 1;
+      const singleBikeId = 2710350;
+      // EXECUTION
+      bikeApiService
+        .getBikesByCity(city, pageNumber, resultsPerPage)
+        .subscribe((bikeSummaries) => {
+          // ASSERT
+          expect(bikeSummaries).toBeTruthy();
+          expect(bikeSummaries.length).toBe(10);
+
+          const bikeSummary = bikeSummaries.find(
+            (bike) => bike.id === singleBikeId,
+          );
+          expect(bikeSummary?.title).toBe("Bike Fun Leonardo");
+        });
+
+      const request = httpTestingController.expectOne(
+        (req) => req.url === `${BIKE_INDEX_V3_API_BASE_URL}/search`,
+      );
+      expect(request.request.method).toBe("GET");
+      // Query param checks
+      expect(request.request.params.get("location")).toBe(city);
+      expect(request.request.params.get("page")).toBe(pageNumber.toString());
+      expect(request.request.params.get("per_page")).toBe(
+        resultsPerPage.toString(),
+      );
+      expectStaticBikeSearchParams(request);
+
+      request.flush({ bikes: bikeSummariesMock });
+    });
+
+    it("should produce a 500 error if the server responds with a 500 error", () => {
+      // SETUP
+      const city = "Amsterdam";
+      const pageNumber = 1;
+      const errorMessage = "Internal server error";
+
+      // EXECUTION
+      bikeApiService
+        .getBikesByCity(city, pageNumber, resultsPerPage)
+        .subscribe({
+          next: () =>
+            fail("the getBikesByCity operation should have produced an error"), // ASSERTION
+          error: (err: HttpErrorResponse) => {
+            // ASSERTION
+            expect(err.status).toBe(500);
+            expect(err.error).toBe(errorMessage);
+          },
+        });
+
+      // ASSERTION
+      const request = httpTestingController.expectOne(
+        (req) => req.url === `${BIKE_INDEX_V3_API_BASE_URL}/search`,
+      );
+      expect(request.request.method).toBe("GET");
+      expect(request.request.params.get("location")).toBe(city);
+      expect(request.request.params.get("page")).toBe(pageNumber.toString());
+      expect(request.request.params.get("per_page")).toBe(
+        resultsPerPage.toString(),
+      );
+      expectStaticBikeSearchParams(request);
+
+      request.flush(errorMessage, { status: 500, statusText: errorMessage }); // EXECUTION
+    });
+  });
+
+  describe("getBikesResultCountByCity()", () => {
+    it("should return a valid bikes result count", () => {
+      // SETUP
+      const city = "Amsterdam";
+      const bikeResultsCount = bikeResultsCountMock.proximity;
+
+      // EXECUTION
+      bikeApiService.getBikesResultCountByCity(city).subscribe((count) => {
+        // ASSERTION
+        expect(count).toBe(bikeResultsCount);
+      });
+
+      // ASSERTION
+      const request = httpTestingController.expectOne(
+        (req) => req.url === `${BIKE_INDEX_V3_API_BASE_URL}/search/count`,
+      );
+      expect(request.request.method).toBe("GET");
+      // Query param check
+      expect(request.request.params.get("location")).toBe(city);
+      expectStaticBikeSearchParams(request);
+
+      request.flush(bikeResultsCountMock); // EXECUTION
+    });
+
+    it("should produce a 500 error if the server responds with a 500 error", () => {
+      // SETUP
+      const city = "Amsterdam";
+      const errorMessage = "Internal server error";
+
+      // EXECUTION
+      bikeApiService.getBikesResultCountByCity(city).subscribe({
+        next: () =>
+          fail(
+            "the getBikesResultCountByCity operation should have produced an error",
+          ),
+        error: (error: HttpErrorResponse) => {
+          expect(error.status).toBe(500);
+          expect(error.error).toBe(errorMessage);
+        },
+      });
+
+      // ASSERTION
+      const request = httpTestingController.expectOne(
+        (req) => req.url === `${BIKE_INDEX_V3_API_BASE_URL}/search/count`,
+      );
+      expect(request.request.method).toBe("GET");
+      // Query param check
+      expect(request.request.params.get("location")).toBe(city);
+      expectStaticBikeSearchParams(request);
+
+      request.flush(errorMessage, {
+        status: 500,
+        statusText: "Internal server error",
+      }); // EXECUTION
+    });
+  });
+
+  describe("getBikeById()", () => {
+    it("should return a bike when given a valid ID", () => {
+      // SETUP
+      const bikeId = 2762265;
+      const bikeTitle = "2025 Giant TCR Advanced 0";
+
+      // EXECUTION
+      bikeApiService.getBikeById(bikeId).subscribe((bikeDetails) => {
+        // ASSERTION
+        expect(bikeDetails).toBeTruthy();
+        expect(bikeDetails.id).toBe(bikeId);
+        expect(bikeDetails.title).toBe(bikeTitle);
+      });
+
+      // ASSERTION
+      const request = httpTestingController.expectOne(
+        `${BIKE_INDEX_V3_API_BASE_URL}/bikes/${bikeId}`,
+      );
+      expect(request.request.method).toBe("GET");
+
+      request.flush({ bike: bikeDetailsMock }); // EXECUTION
+    });
+
+    it("should produce a 404 error when given an invalid ID", () => {
+      // SETUP
+      const bikeId = 2389472093472309;
+      const errorMessage = "Couldn't find Bike with 'id'=2389472093472309";
+
+      // EXECUTION
+      bikeApiService.getBikeById(bikeId).subscribe({
+        next: () =>
+          fail("the getBikeById operation should have produced an error"),
+        error: (error: HttpErrorResponse) => {
+          // ASSERTION
+          expect(error.status).toBe(404);
+          expect(error.error).toBe(errorMessage);
+        },
+      });
+
+      const request = httpTestingController.expectOne(
+        `${BIKE_INDEX_V3_API_BASE_URL}/bikes/${bikeId}`,
+      );
+      expect(request.request.method).toBe("GET");
+
+      request.flush(errorMessage, {
+        status: 404,
+        statusText: "No page found",
+      }); // EXECUTION
+    });
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 });
---
+++ src/app/features/bikes/testing/mocks/bike-details.mock.ts
@@ -0,0 +1,98 @@
+export const bikeDetailsMock = {
+  date_stolen: 1741352400,
+  description:
+    "Size: M/L\r\nColor: Frost Silver\r\nDi2 shifters\r\nAssioma Favero Duo-SH powermeter\r\nGarmin Edge 1040 Bike computer\r\nFront/back lights (detachable)",
+  frame_colors: ["Blue", "Silver, gray or bare metal"],
+  frame_model: "TCR Advanced 0",
+  id: 2762265,
+  is_stock_img: false,
+  large_img:
+    "https://files.bikeindex.org/uploads/Pu/895431/large_046AA731-F92A-4126-9556-F0224EA52884_1_105_c.jpeg",
+  location_found: null,
+  manufacturer_name: "Giant",
+  external_id: null,
+  registry_name: null,
+  registry_url: null,
+  serial: "GBJF02796",
+  status: "stolen",
+  stolen: true,
+  stolen_coordinates: [52.35, 4.92],
+  stolen_location: "Amsterdam, 1091KX, NL",
+  thumb:
+    "https://files.bikeindex.org/uploads/Pu/895431/small_046AA731-F92A-4126-9556-F0224EA52884_1_105_c.jpeg",
+  title: "2025 Giant TCR Advanced 0",
+  url: "https://bikeindex.org/bikes/2762265",
+  year: 2025,
+  propulsion_type_slug: "foot-pedal",
+  cycle_type_slug: "bike",
+  registration_created_at: 1741353824,
+  registration_updated_at: 1741375770,
+  api_url: "https://bikeindex.org/api/v1/bikes/2762265",
+  manufacturer_id: 153,
+  paint_description: null,
+  name: null,
+  frame_size: "m",
+  rear_tire_narrow: true,
+  front_tire_narrow: null,
+  type_of_cycle: "Bike",
+  test_bike: false,
+  rear_wheel_size_iso_bsd: null,
+  front_wheel_size_iso_bsd: null,
+  handlebar_type_slug: null,
+  frame_material_slug: "composite",
+  front_gear_type_slug: null,
+  rear_gear_type_slug: null,
+  extra_registration_number: null,
+  additional_registration: null,
+  stolen_record: {
+    date_stolen: 1741352400,
+    location: "Amsterdam, 1091KX, NL",
+    latitude: 52.35,
+    longitude: 4.92,
+    theft_description:
+      "Ballustrade van (afgesloten) appartementencomplex (3e verdieping)",
+    locking_description: "",
+    lock_defeat_description: "",
+    police_report_number: "PL1300-2025054445-2",
+    police_report_department: "Amsterdam",
+    created_at: 1741353824,
+    create_open311: false,
+    id: 187470,
+  },
+  public_images: [
+    {
+      name: "2025 Giant TCR Advanced 0 Blue en Silver, gray or bare metal",
+      full: "https://files.bikeindex.org/uploads/Pu/895431/046AA731-F92A-4126-9556-F0224EA52884_1_105_c.jpeg",
+      large:
+        "https://files.bikeindex.org/uploads/Pu/895431/large_046AA731-F92A-4126-9556-F0224EA52884_1_105_c.jpeg",
+      medium:
+        "https://files.bikeindex.org/uploads/Pu/895431/medium_046AA731-F92A-4126-9556-F0224EA52884_1_105_c.jpeg",
+      thumb:
+        "https://files.bikeindex.org/uploads/Pu/895431/small_046AA731-F92A-4126-9556-F0224EA52884_1_105_c.jpeg",
+      id: 895431,
+    },
+    {
+      name: "2025 Giant TCR Advanced 0 Blue en Silver, gray or bare metal",
+      full: "https://files.bikeindex.org/uploads/Pu/895432/148004F2-7DC8-4152-8596-1E14FA5CB0F5_1_105_c.jpeg",
+      large:
+        "https://files.bikeindex.org/uploads/Pu/895432/large_148004F2-7DC8-4152-8596-1E14FA5CB0F5_1_105_c.jpeg",
+      medium:
+        "https://files.bikeindex.org/uploads/Pu/895432/medium_148004F2-7DC8-4152-8596-1E14FA5CB0F5_1_105_c.jpeg",
+      thumb:
+        "https://files.bikeindex.org/uploads/Pu/895432/small_148004F2-7DC8-4152-8596-1E14FA5CB0F5_1_105_c.jpeg",
+      id: 895432,
+    },
+    {
+      name: "2025 Giant TCR Advanced 0 Blue en Silver, gray or bare metal",
+      full: "https://files.bikeindex.org/uploads/Pu/895433/6400C472-87C4-4B6C-9335-211E5E676631_1_105_c.jpeg",
+      large:
+        "https://files.bikeindex.org/uploads/Pu/895433/large_6400C472-87C4-4B6C-9335-211E5E676631_1_105_c.jpeg",
+      medium:
+        "https://files.bikeindex.org/uploads/Pu/895433/medium_6400C472-87C4-4B6C-9335-211E5E676631_1_105_c.jpeg",
+      thumb:
+        "https://files.bikeindex.org/uploads/Pu/895433/small_6400C472-87C4-4B6C-9335-211E5E676631_1_105_c.jpeg",
+      id: 895433,
+    },
+  ],
+  components: [],
+};
